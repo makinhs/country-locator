c64b88ab1f49212c01001fe4b25e4db539678c4c
Marcos Silva <tio.makin@gmail.com>
2021-09-02 13:51:48 +0200

    promise test

--- src/find-methods/find-countries-by-polygon.ts
+++ src/find-methods/find-countries-by-polygon.ts
@@ -5,25 +5,25 @@ import {COUNTRIES_GEO_JSON, NOT_A_COUNTRY_CODE} from "../constants/all-countries
 import {validatePolygon} from "../validation/polygon-validation";
 
 const countryIntersectsWithPolygon = (country: CountryFeature, polygon: Feature<Polygon>): boolean => {
-    const {properties, geometry} = country;
-    if (properties.ISO_A3 === NOT_A_COUNTRY_CODE) return false;
-    if (geometry.type === 'Polygon') {
-        try {
-            return intersect(createPolygon(geometry.coordinates), polygon) !== null;
-        } catch {
-            return false;
-        }
-    } else if (geometry.type === 'MultiPolygon') {
-        return geometry.coordinates.some((coordinate: any) => {
-            try {
-                const countryTurfPolygon = createPolygon(coordinate);
-                return intersect(countryTurfPolygon, polygon) !== null;
-            } catch {
-                return false;
-            }
-        });
+  const {properties, geometry} = country;
+  if (properties.ISO_A3 === NOT_A_COUNTRY_CODE) return false;
+  if (geometry.type === 'Polygon') {
+    try {
+      return intersect(createPolygon(geometry.coordinates), polygon) !== null;
+    } catch {
+      return false;
     }
-    return false;
+  } else if (geometry.type === 'MultiPolygon') {
+    return geometry.coordinates.some((coordinate: any) => {
+      try {
+        const countryTurfPolygon = createPolygon(coordinate);
+        return intersect(countryTurfPolygon, polygon) !== null;
+      } catch {
+        return false;
+      }
+    });
+  }
+  return false;
 };
 
 /**
@@ -33,25 +33,28 @@ const countryIntersectsWithPolygon = (country: CountryFeature, polygon: Feature<
  * with the given polygon. empty array if the polygon doesn't intersect anything.
  */
 export function findCountriesByPolygon(polygon: number[][]): CountryInfo[] {
-    const validPolygon = validatePolygon(polygon);
-    const countriesInIntersection = COUNTRIES_GEO_JSON.features
-        .filter(country => countryIntersectsWithPolygon(country, validPolygon));
-    return countriesInIntersection.map(country => ({
-        name: country.properties.ADMIN,
-        code: country.properties.ISO_A3
-    }));
+  const validPolygon = validatePolygon(polygon);
+  const countriesInIntersection = COUNTRIES_GEO_JSON.features
+    .filter(country => countryIntersectsWithPolygon(country, validPolygon));
+  return countriesInIntersection.map(country => ({
+    name: country.properties.ADMIN,
+    code: country.properties.ISO_A3,
+  }));
 }
 
-export function findCountriesByPolygonV2(polygon: number[][]): CountryInfo[] {
+export function findCountriesByPolygonV2(polygon: number[][]): Promise<CountryInfo[]> {
+  return new Promise((resolve) => {
     const validPolygon = validatePolygon(polygon);
     const countriesInIntersection = [];
-    for(const country of COUNTRIES_GEO_JSON.features){
-        if(countryIntersectsWithPolygon(country, validPolygon)){
-            countriesInIntersection.push(country)
-        }
+    for (const country of COUNTRIES_GEO_JSON.features) {
+      if (countryIntersectsWithPolygon(country, validPolygon)) {
+        countriesInIntersection.push(country)
+      }
     }
-    return countriesInIntersection.map(country => ({
-        name: country.properties.ADMIN,
-        code: country.properties.ISO_A3
-    }));
+    resolve(countriesInIntersection.map(country => ({
+      name: country.properties.ADMIN,
+      code: country.properties.ISO_A3,
+    })));
+  })
+
 }
